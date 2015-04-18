a070cbfe2df7367b24229493b11cd85fa9d04951
JeffHemming <hemmingjm@gmail.com>
2015-04-18 13:14:18 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+/**
+ * Created by Dad on 4/18/2015.
+ */
+public class Main {
+
+    public static void main(String [] args) throws IOException {
+        LevelGUI lg=new LevelGUI(1);
+
+    }
+}
